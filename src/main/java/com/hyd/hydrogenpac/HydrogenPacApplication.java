6363a8447a3140c6e235e5bbe90012f4b678db89
Yiding He <yiding.he@gmail.com>
2018-09-18 19:11:29 +0800

    update configuration

--- src/main/java/com/hyd/hydrogenpac/HydrogenPacApplication.java
+++ src/main/java/com/hyd/hydrogenpac/HydrogenPacApplication.java
@@ -1,12 +1,19 @@
 package com.hyd.hydrogenpac;
 
+import com.hyd.hydrogenpac.config.AuthConfig;
+import com.hyd.hydrogenpac.config.CookieConfig;
+import com.hyd.hydrogenpac.config.DbConfig;
 import org.dizitart.no2.Nitrite;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        DbConfig.class, AuthConfig.class, CookieConfig.class
+})
 public class HydrogenPacApplication {
 
     @Autowired
--- src/main/java/com/hyd/hydrogenpac/config/CookieConfig.java
+++ src/main/java/com/hyd/hydrogenpac/config/CookieConfig.java
@@ -1,9 +1,7 @@
 package com.hyd.hydrogenpac.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-@Component
 @ConfigurationProperties(prefix = "cookie")
 public class CookieConfig {
 
