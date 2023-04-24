c9526a3905c3d2a780c0dcfbc4adebebe19ac1ee
Yiding He <yiding.he@gmail.com>
2023-04-24 16:30:46 +0800

    添加 JVM 参数

--- build-app-image.cmd
+++ build-app-image.cmd
@@ -1,6 +1,8 @@
+rem 附加的内存参数能将运行时内存使用量降到 140M 左右
 %JDK_20%\bin\jpackage.exe --name hydrogen-pac ^
   --input target\dist ^
   --main-jar hydrogen-pac-0.0.1-SNAPSHOT.jar ^
   --type app-image ^
   --dest target^
-  --icon .\assets\windows\hydrogen-pac.ico
+  --icon .\assets\windows\hydrogen-pac.ico^
+  --java-options "-XX:MaxMetaspaceSize=40m -XX:ReservedCodeCacheSize=15m -Xmx30m -XX:CompressedClassSpaceSize=7m -XX:NewSize=3m -XX:MaxNewSize=8m"
